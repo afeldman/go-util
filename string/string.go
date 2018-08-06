35880c3fe9342228fd6887b7bb1042b4920a679f
anton feldmann <anton.feldmann@gmail.com>
2018-08-06 18:51:40 +0900

    string util

---
+++ string/string.go
@@ -0,0 +1,22 @@
+package string
+
+import "unicode"
+
+// Strempty checks whether string contains only whitespace or not
+func StringEmpty(s string) bool {
+	if len(s) == 0 {
+		return true
+	}
+
+	r := []rune(s)
+	l := len(r)
+
+	for l > 0 {
+		l--
+		if !unicode.IsSpace(r[l]) {
+			return false
+		}
+	}
+
+	return true
+}
