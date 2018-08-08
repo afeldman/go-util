d7fbfe6992e58776b00df49021985c4ed8a2c73f
anton feldmann <anton.feldmann@gmail.com>
2018-08-08 13:50:52 +0900

    copy return error

--- file/copy.go
+++ file/copy.go
@@ -6,25 +6,25 @@ import (
 	"log"
 )
 
-func Fcopy(src, dest string) {
+func Fcopy(src, dest string) error{
 	// Open original file
 	originalFile, err := os.Open(src)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	defer originalFile.Close()
 
 	// Create new file
 	newFile, err := os.Create(dest)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	defer newFile.Close()
 
 	// Copy the bytes to destination from source
 	bytesWritten, err := io.Copy(newFile, originalFile)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	log.Printf("Copied %d bytes.", bytesWritten)
 
@@ -32,6 +32,8 @@ func Fcopy(src, dest string) {
 	// Flushes memory to disk
 	err = newFile.Sync()
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
+
+	return nil
 }
