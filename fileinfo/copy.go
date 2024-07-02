dfcb522a906a2794b025b8b26930309b7ef975d1
Anton Feldmann <anton.feldmann@gmail.com>
2024-07-02 10:08:26 +0200

    fileinfo

---
+++ fileinfo/copy.go
@@ -0,0 +1,39 @@
+package fileinfo
+
+import (
+	"io"
+	"log"
+	"os"
+)
+
+func Fcopy(src, dest string) error {
+	// Open original file
+	originalFile, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer originalFile.Close()
+
+	// Create new file
+	newFile, err := os.Create(dest)
+	if err != nil {
+		return err
+	}
+	defer newFile.Close()
+
+	// Copy the bytes to destination from source
+	bytesWritten, err := io.Copy(newFile, originalFile)
+	if err != nil {
+		return err
+	}
+	log.Printf("Copied %d bytes.", bytesWritten)
+
+	// Commit the file contents
+	// Flushes memory to disk
+	err = newFile.Sync()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ fileinfo/stats.go
@@ -0,0 +1,11 @@
+package fileinfo
+
+import "os"
+
+func DirExists(path string) bool {
+
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
