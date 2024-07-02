b43879905b56089e59dfd0c779a7e5e4484f3809
Anton Feldmann <anton.feldmann@gmail.com>
2024-07-02 10:22:54 +0200

    publuc functions

--- crypto/crypto.go
+++ crypto/crypto.go
@@ -1,42 +1,75 @@
+/*
+package crypto
+This package provides functions to calculate the checksum of a file
+*/
 package crypto
 
 import (
-  "crypto/sha256"
-  "crypto/md5"
-  "encoding/hex"
-  "io"
-  "os"
+	"crypto/md5"
+	"crypto/sha256"
+	"encoding/hex"
+	"io"
+	"os"
 )
 
-func sha256sum(filePath string) (result string, err error) {
-  f, err := os.Open(filePath)
-  if err != nil {
-    return
-  }
-  defer f.Close()
+/*
+	Calculate the SHA256 checksum of a file
 
-  h := sha256.New()
-  if _, err = io.Copy(h, f); err != nil {
-    return
-  }
+Arguments:
 
-  result = hex.EncodeToString(h.Sum(nil))
-  return 
+	filePath -- the path to the file to calculate the checksum for
+
+Returns:
+
+	string -- the SHA256 checksum of the file
+	error -- an error if the file could not be read
+*/
+func SHA256sum(filePath string) (result string, err error) {
+	// Open the file
+	f, err := os.Open(filePath)
+	if err != nil {
+		return
+	}
+	defer f.Close()
+
+	// Create a new SHA256 hash
+	h := sha256.New()
+
+	// Copy the file contents to the hash
+	if _, err = io.Copy(h, f); err != nil {
+		return
+	}
+
+	// Get the checksum
+	result = hex.EncodeToString(h.Sum(nil))
+	return
 }
 
-func md5sum(filePath string) (result string, err error) {
-    file, err := os.Open(filePath)
-    if err != nil {
-        return 
-    }
-    defer file.Close()
+/*
+	md5 file hash
 
-    hash := md5.New()
-    _, err = io.Copy(hash, file)
-    if err != nil {
-        return 
-    }
+Arguments:
 
-    result = hex.EncodeToString(hash.Sum(nil))
-    return 
+	filePath -- the path to the file to calculate the checksum for
+
+Returns:
+
+	string -- the md5 checksum of the file
+	error -- an error if the file could not be read
+*/
+func MD5sum(filePath string) (result string, err error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return
+	}
+	defer file.Close()
+
+	hash := md5.New()
+	_, err = io.Copy(hash, file)
+	if err != nil {
+		return
+	}
+
+	result = hex.EncodeToString(hash.Sum(nil))
+	return
 }
--- env/env.go
+++ env/env.go
@@ -1,3 +1,6 @@
+// package env
+// This package provides functions to get environment variables
+
 package env
 
 import (
@@ -5,14 +8,47 @@ import (
 	"strings"
 )
 
+/*
+	plan9 format environment variable
+
+Plan 9 uses a different format for environment variables.
+It uses a single string with the format key=value separated by null bytes.
+This function returns the value of the environment variable in Plan 9 format.
+
+Arguments:
+
+	env_var -- the name of the environment variable to get
+
+Returns:
+
+	string -- the value of the environment variable
+*/
 func Plan9GetEnv(env_var string) string {
 	return GetEnvOrDefault(env_var, strings.ToLower(env_var))
 }
 
+/*
+	are we running on Windows?
+
+Returns:
+
+	bool -- true if the OS is Windows, false otherwise
+*/
 func IsWindows() bool {
 	return os.PathSeparator == '\\' && os.PathListSeparator == ';'
 }
 
+/*
+	Get the value of an environment variable
+
+Arguments:
+
+	env_var -- the name of the environment variable to get
+
+Returns:
+
+	[]string -- the value of the environment variable
+*/
 func GetEnv(env_var string) []string {
 	if IsWindows() {
 		return strings.Split(os.Getenv(env_var), ";")
@@ -21,6 +57,18 @@ func GetEnv(env_var string) []string {
 	}
 }
 
+/*
+	get the env value or default if not exists
+
+Arguments:
+
+	name -- the name of the environment variable to get
+	or -- the default value to return if the environment variable does not exist
+
+Returns:
+
+	string -- the value of the environment variable or the default value if it does not exist
+*/
 func GetEnvOrDefault(name, or string) string {
 	if value, ok := os.LookupEnv(name); ok {
 		return value
