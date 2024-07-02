d2a6b19b859e7bd700d4521224d166beed5ab9e6
Anton Feldmann <anton.feldmann@gmail.com>
2024-07-02 11:25:23 +0200

    errorchecker

--- check/error_check.go
+++ check/error_check.go
@@ -3,8 +3,21 @@ package check
 
 import log "github.com/sirupsen/logrus"
 
-func CheckError(err error) {
+// CheckError: check for errors
+// err: error
+// return: bool
+func CheckError(err error) bool {
 	if err != nil {
-		log.Fatal(err)
+		log.Error(err.Error())
+		return false
+	}
+	return true
+}
+
+// CheckErrorFatal: check for errors
+// err: error
+func CheckErrorFatal(err error) {
+	if err != nil {
+		log.Fatal(err.Error())
 	}
 }
